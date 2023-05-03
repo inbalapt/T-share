bf6c1ce7e3264ca30d5c70a6266989323b4c87aa
noaLeshem2 <noaleshem99@gmail.com>
2023-05-03 13:28:30 +0300

    aaa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import { Link, BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
+import Item from './Item';
+import Test from './Test';
+//import FrontTab from './FrontTab';
+import HeaderTop from './HeaderTop';
+import NavigationBar from './NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LoginPage />}/>
+          <Route path='/' element={<Test />}/>
           <Route path='/RegisterPage' element={<RegisterPage />}/>
         </Routes>
   </BrowserRouter>
