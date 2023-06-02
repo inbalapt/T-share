2fbd393bf6694f53918c09e08a42d6dd470cf916
noaLeshem2 <noaleshem99@gmail.com>
2023-06-02 15:47:59 +0300

    designed hame page and logo

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,25 +1,64 @@
 import React from 'react';
 import './ItemScrollPage.css';
 import NavigationBar from './NavigationBar';
-//import Item from './Item';
-//import logo from './logo.jpg'
-import { useParams ,useLocation} from 'react-router-dom';
-//import { useState, useEffect } from 'react';
-
+import { useNavigate, useLocation } from "react-router-dom";
+import './HomePage.css';
 
 const HomePage = () => {
     const location = useLocation();
+    let navigate = useNavigate();
     const username = location.state.username;
-    //let img = `/pictures/home.jpg`;
+
+    function handleCategory(category){
+        navigate(`../clothing/${category}`, {state: {username: username}})
+    }
 
     return (
-        <div>
-          <NavigationBar username={username}/>
-          <div className="category-header" style={{ backgroundImage:`url(${`/pictures/home.jpg`})`}}>
-            <h1>Welcome</h1>
+        <div className="home-page">
+            <NavigationBar username={username}/>
+            <div className='parent-cat-head'> 
+                <div className="category-header" style={{ backgroundImage:`url(${`/pictures/home.jpg`})`}}>
+                    <h1 className='head-home'>Step into Sustainable Style: Welcome to T-Share</h1>
+                </div>
+            </div>
+
+            <div className='home-categories'>
+                <div 
+                    className='category-dress' 
+                    style={{ backgroundImage:`url(${`/pictures/dress-home.jpg`})`}}
+                    onClick={() => handleCategory('dresses')}
+                >
+                    <div className="category-label">Buy Dresses</div>
+                </div>
+
+                <div 
+                    className='category-top' 
+                    style={{ backgroundImage:`url(${`/pictures/top-home.jpg`})`}}
+                    onClick={() => handleCategory('tops')}
+                >
+                    <div className="category-label">Buy Tops</div>
+                </div>
+
+                <div className='skirt-pants'>
+                    <div 
+                        className='category-skirt' 
+                        style={{ backgroundImage:`url(${`/pictures/skirt-home.jpg`})`}}
+                        onClick={() => handleCategory('skirts')}
+                    >
+                        <div className="category-label">Buy Skirts</div>
+                    </div>
+
+                    <div 
+                        className='category-pants' 
+                        style={{ backgroundImage:`url(${`/pictures/pants-home.jpg`})`}}
+                        onClick={() => handleCategory('pants')}
+                    >
+                        <div className="category-label">Buy Pants</div>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default HomePage;
